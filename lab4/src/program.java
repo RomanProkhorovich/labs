504b3a3735892d40da8c8c609b4aed58cf7af5b8
Roman <pomaprokhorovich583@gmail.com>
2022-10-28 04:26:16 +0400

    Ok

--- lab4/src/program.java
+++ lab4/src/program.java
@@ -3,8 +3,17 @@ import task5.*;
 import java.io.*;
 
 public class program {
-    public static void main(String args[]) throws DuplicateModelNameException {
+    public static void main(String args[]) throws DuplicateModelNameException, IOException, ClassNotFoundException {
+        FileOutputStream fio=new FileOutputStream("D:\\labs\\lab3\\src\\\\task5\\textFiles\\numeric.txt");
+        ITransport tr=new Car("name",10);
+        Transport.printInfo(tr);
+        ObjectOutputStream ob=new ObjectOutputStream(fio);
+        ob.writeObject(tr);
+        System.out.println();
 
-
+        FileInputStream fis=new FileInputStream("D:\\labs\\lab3\\src\\\\task5\\textFiles\\numeric.txt");
+        ObjectInputStream ins=new ObjectInputStream(fis);
+        ITransport car=(Car)ins.readObject();
+        Transport.printInfo(car);
     }
 }
